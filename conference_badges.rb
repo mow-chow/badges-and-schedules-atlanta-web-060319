c95d33ce54498ed5dec74e18e68e38b28e847938
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-13 01:14:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,7 @@ end
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
-
-  speakerss = []
+  speakersbadge = []
   speakers.each do |name|
     speakerss.push("Hello, my name is #{name}.")
   end
