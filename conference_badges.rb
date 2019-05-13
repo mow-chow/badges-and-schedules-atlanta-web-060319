fb2df7de60186eb3094b922a22fd028858a1e150
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-13 01:06:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ end
 
 def batch_badge_creator(speakers)
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  return speakers.each {|i| puts "Hello, my name is #{speakers}"}
+  speakers_badge = []
 end
 
 def assign_rooms(speakers)
