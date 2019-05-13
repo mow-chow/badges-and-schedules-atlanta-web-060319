6709bd76b54d062be0ab4d2899c2c5b1d69fe6cf
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-13 01:16:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,9 @@ end
 
 def assign_rooms(speakers)
   speakersbr = []
+  counter = 1
   speakers.each do |name|
+    speakersbr.push("Hello, #{name}! You'll be assigned to room 1!")
 end
 
 def printer
